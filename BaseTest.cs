0c19e8053dd52dd4a771c78f40460ab74707cad5
Kevin <none>
2021-12-24 18:12:41 -0500

    Setup test data for xunit.

--- BaseTest.cs
+++ BaseTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Xunit;
+using Xunit.Sdk;
 
 namespace ApiSample;
 
@@ -14,5 +16,29 @@ public class BaseTest
     {
         _client = new ApiAccessor();
     }
+
+    public static TheoryData<Post> PostData =>
+        new TheoryData<Post>
+        {
+             new Post(1,5,String.Empty,  LoremNET.Lorem.Words(3) ),
+            new Post(2,  20,LoremNET.Lorem.Sentence(3, 20), 
+                LoremNET.Lorem.Words(3) )
+        };
+
+    public static TheoryData<Post> NewPostData =>
+        new TheoryData<Post>
+        {
+            new Post(1, null, LoremNET.Lorem.Sentence(3, 20),
+                LoremNET.Lorem.Paragraph(10, 4)),
+            new Post(2, null, LoremNET.Lorem.Sentence(3, 20),
+                LoremNET.Lorem.Paragraph(10, 4))
+        };
+
+    public static TheoryData<Comment> NewCommentData =>
+        new TheoryData<Comment>
+        {
+            new Comment(2, null, LoremNET.Lorem.Words(3), LoremNET.Lorem.Email(),
+                LoremNET.Lorem.Sentence(3, 30))
+        };
 }
 
